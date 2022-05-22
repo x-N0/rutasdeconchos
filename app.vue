daf37ce344b3e4cfb31efae4b40a65e8a2828bc2
x-N0 <51316342+x-N0@users.noreply.github.com>
2022-05-22 07:19:53 -0400

    Initial commit.
    Init Nuxt 3.
    Add leaflet map pointing to DR.
    Add tailwindcss.
    Add gtag for GA tracking.

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-screen w-screen flex-col">
+  <h1 class="text-3xl font-bold underline text-center">
+    [WIP] Simple SSR Map to show routes.
+  </h1>
+  <leaflet-map/>
+  </div>
+</template>
+<script lang="ts">
+</script>
---
+++ components/leaflet-map.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { LMap, LTileLayer, LControlLayers } from '@vue-leaflet/vue-leaflet'
+const zoom = 9;
+const drCenter = [18.8050403, -70.6763191]
+</script>
+<template>
+    <LMap v-model="zoom" v-model:zoom="zoom" :center="drCenter">
+        <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></LTileLayer>
+    </LMap>
+</template>
